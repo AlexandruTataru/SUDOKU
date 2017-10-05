12a61b3947ae7c5c9eff3d59f814efa888d67bd4
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-10-05 17:59:10 +0300

    Extra implementation for the C++ binding

--- Alex_CppBinding/CppBinding/solver.cpp
+++ Alex_CppBinding/CppBinding/solver.cpp
@@ -4,7 +4,50 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <set>
+#include <algorithm>
+#include <numeric>
+
 uint8_t board[9][9];
+
+bool internal_isRowValid(uint8_t row)
+{
+	std::set<uint8_t> values;
+	for (uint8_t column = 0; column < 9; ++column) values.insert(board[row][column]);
+	return std::accumulate(values.begin(), values.end(), 0) == 45;
+}
+
+bool internal_isColumnValid(uint8_t column)
+{
+	std::set<uint8_t> values;
+	for (uint8_t row = 0; row < 9; ++row) values.insert(board[row][column]);
+	return std::accumulate(values.begin(), values.end(), 0) == 45;
+}
+
+bool internal_isBoardSolved()
+{
+	size_t sum = 0;
+	int row = 0, column = 0;
+	for (; row < 9; ++row) for (column = 0; column < 9; ++column) sum += board[row][column];
+
+	if (sum != 405) return false;
+
+	for (uint8_t idx = 0; idx < 9; ++idx) if (!internal_isRowValid(idx) || !internal_isColumnValid(idx)) return false;
+
+	return true;
+}
+
+bool internal_hasColumnDigit(uint8_t colNr, uint8_t digit)
+{
+	for (uint8_t row = 0; row < 9; ++row) if (board[row][colNr] == digit) return true;
+	return false;
+}
+
+bool internal_hasRowDigit(uint8_t rowNr, uint8_t digit)
+{
+	for (uint8_t column = 0; column < 9; ++column) if (board[rowNr][column] == digit) return true;
+	return false;
+}
 
 bool loadBoard(uint8_t *incomingBoard)
 {
@@ -39,6 +82,10 @@ bool loadBoard(uint8_t *incomingBoard)
 
 bool solveBoard()
 {
+	while (!internal_isBoardSolved())
+	{
+
+	}
 	return true;
 }
 
