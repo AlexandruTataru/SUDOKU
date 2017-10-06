aae2d6a59e59759bbfbb98e838d50d54a7382075
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-10-06 15:11:44 +0300

    Organized project structure

--- Alex_CppBinding/CppBinding/API.cpp
+++ Alex_CppBinding/CppBinding/API.cpp
@@ -1,4 +1,4 @@
-#include "solver.h"
+#include "API.h"
 
 #include <windows.h>
 #include <stdio.h>
@@ -76,9 +76,9 @@ bool internal_hasRowDigit(uint8_t rowNr, uint8_t digit)
 
 bool loadBoard(uint8_t *incomingBoard)
 {
-	AllocConsole();
-	AttachConsole(GetCurrentProcessId());
-	freopen("CON", "w", stdout);
+	//AllocConsole();
+	//AttachConsole(GetCurrentProcessId());
+	//freopen("CON", "w", stdout);
 
 	size_t i = 0;
 	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) board[row][column] = incomingBoard[i++];
