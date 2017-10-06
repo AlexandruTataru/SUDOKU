183541750b1c13f84b8a9a56afcf78b798f9675f
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-10-06 14:14:06 +0300

    Added final implementation for final solver

--- Alex_CppBinding/CppBinding/solver.cpp
+++ Alex_CppBinding/CppBinding/solver.cpp
@@ -7,8 +7,15 @@
 #include <set>
 #include <algorithm>
 #include <numeric>
+#include <vector>
 
 uint8_t board[9][9];
+
+const uint8_t CLEAR_TILE = 0;
+const uint8_t MARKED_TILE = 1;
+
+typedef std::pair<uint8_t, uint8_t> position;
+typedef std::vector<position> positions;
 
 bool internal_isRowValid(uint8_t row)
 {
@@ -22,6 +29,24 @@ bool internal_isColumnValid(uint8_t column)
 	std::set<uint8_t> values;
 	for (uint8_t row = 0; row < 9; ++row) values.insert(board[row][column]);
 	return std::accumulate(values.begin(), values.end(), 0) == 45;
+}
+
+void printBoard(uint8_t board[][9])
+{
+	uint8_t row, column;
+	for (row = 0; row < 9; ++row)
+	{
+		for (column = 0; column < 9; ++column)
+		{
+			printf("%d ", board[row][column]);
+		}
+		printf("\n");
+	}
+}
+
+void resetBoard(uint8_t board[][9], uint8_t resetDigit)
+{
+	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) board[row][column] = resetDigit;
 }
 
 bool internal_isBoardSolved()
@@ -55,45 +80,121 @@ bool loadBoard(uint8_t *incomingBoard)
 	AttachConsole(GetCurrentProcessId());
 	freopen("CON", "w", stdout);
 
-	size_t row = 0, column = 0;
-	for (int i = 0; i < 81; ++i)
-	{
-		board[row][column] = incomingBoard[i];
-		column++;
-		if (column == 9)
-		{
-			column = 0;
-			row++;
-		}
-	}
+	size_t i = 0;
+	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) board[row][column] = incomingBoard[i++];
 
-	row = 0; column = 0;
-	for (; row < 9; ++row)
-	{
-		for (column = 0; column < 9; ++column)
-		{
-			printf("%d ", board[row][column]);
-		}
-		printf("\n");
-	}
+	//printBoard(board);
 
 	return true;
+}
+
+void markAlreadyUsedLocations(uint8_t tempBoard[][9])
+{
+	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) if (board[row][column]) tempBoard[row][column] = 1;
+}
+
+positions digitLocations(uint8_t digit)
+{
+	positions locations;
+	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) if (board[row][column] == digit) locations.push_back(std::make_pair(row, column));
+	return locations;
+}
+
+void markPositionDown(uint8_t tempBoard[][9], position pos)
+{
+	for (uint8_t idx = 0; idx < 9;++idx)
+	{
+		tempBoard[pos.first][idx] = MARKED_TILE;
+		tempBoard[idx][pos.second] = MARKED_TILE;
+	}
+
+	uint8_t startRow, endRow;
+	if (pos.first <= 2) { startRow = 0; endRow = 2; }
+	else if (pos.first >= 3 && pos.first <= 5) { startRow = 3; endRow = 5; }
+	else if (pos.first >= 6 && pos.first <= 8) { startRow = 6; endRow = 8; }
+
+	uint8_t startCol, endCol;
+	if (pos.second <= 2) { startCol = 0; endCol = 2; }
+	else if (pos.second >= 3 && pos.second <= 5) { startCol = 3; endCol = 5; }
+	else if (pos.second >= 6 && pos.second <= 8) { startCol = 6; endCol = 8; }
+
+	for (uint8_t row = startRow; row <= endRow; ++row)
+	{
+		for (uint8_t column = startCol; column <= endCol; ++column)
+		{
+			tempBoard[row][column] = MARKED_TILE;
+		}
+	}
+}
+
+bool digitIsSingleOnRow(uint8_t tempBoard[][9], uint8_t row, uint8_t currentDigit, position& pos)
+{
+	int found = 0;
+	for (uint8_t column = 0; column < 9; ++column)
+	{
+		if (tempBoard[row][column] == 0)
+		{
+			pos = std::make_pair(row, column);
+			found++;
+		}
+	}
+
+	return found == 1;
+}
+
+bool digitIsSingleOnColumn(uint8_t tempBoard[][9], uint8_t column, uint8_t currentDigit, position& pos)
+{
+	int found = 0;
+	for (uint8_t row = 0; row < 9; ++row)
+	{
+		if (tempBoard[row][column] == 0)
+		{
+			pos = std::make_pair(row, column);
+			found++;
+		}
+	}
+
+	return found == 1;
 }
 
 bool solveBoard()
 {
 	while (!internal_isBoardSolved())
 	{
+		uint8_t tempBoard[9][9];
+		for (uint8_t currentDigit = 1; currentDigit <= 9; ++currentDigit)
+		{
+			//printf("Looking over digit %d\n", currentDigit);
+			resetBoard(tempBoard, CLEAR_TILE);
+			markAlreadyUsedLocations(tempBoard);
+			positions digitPositions = digitLocations(currentDigit);
+			for (auto position : digitPositions)
+			{
+				markPositionDown(tempBoard, position);
+			}
+			//printf("Marked down board looks like this\n");
+			//printBoard(tempBoard);
+			for (uint8_t entry = 0; entry < 9; ++entry)
+			{
+				position pos;
+				if (digitIsSingleOnRow(tempBoard, entry, currentDigit, pos) || digitIsSingleOnColumn(tempBoard, entry, currentDigit, pos))
+				{
+					//printf("Found certain location for digit %d at r:%d - c:%d\n", currentDigit, pos.first, pos.second);
+					board[pos.first][pos.second] = currentDigit;
+				}
+			}
 
+			//printf("Board looks like this\n");
+			//printBoard(board);
+		}
 	}
 	return true;
 }
 
 bool retrieveSolvedBoard(uint8_t* solvedBoard)
 {
-	for (uint8_t i = 0; i < 81; ++i)
-	{
-		solvedBoard[i] = 5;
-	}
+	size_t i = 0;
+	for (uint8_t row = 0; row < 9; ++row) for (uint8_t column = 0; column < 9; ++column) solvedBoard[i++] = board[row][column];
+
 	return true;
 }
