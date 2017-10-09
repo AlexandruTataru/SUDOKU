725ffb119c3173efa470462f4251ce11e183ad27
Tataru Alexandru Flavian <alexandruflavian@gmail.com>
2017-10-09 15:48:32 +0300

    Re-organized project and made a more object orientated design.

---
+++ Alex_CppBinding/CppBinding/Console.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <windows.h>
+#include <iostream>
+#include <stdio.h>
+
+class Console
+{
+public:
+	Console()
+	{
+		AllocConsole();
+		AttachConsole(GetCurrentProcessId());
+		freopen("CON", "w", stdout);
+	}
+
+	~Console()
+	{
+		FreeConsole();
+	}
+
+	template<class T>
+	Console& operator << (const T& obj)
+	{
+		std::cout << obj;
+		return *this;
+	}
+};
